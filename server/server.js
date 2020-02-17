e0515a1b818fad3f667c098eadb045d444094425
yudahov <yudahov@gmail.com>
2020-02-17 17:19:59 +0200

    added mongolab

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ const config = require("./config/config").get(process.env.NODE_ENV);
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(config.DATABASE);
+mongoose.connect(config.DATABASE); 
 
 const { User } = require("./models/user");
 const { Book } = require("./models/book");
